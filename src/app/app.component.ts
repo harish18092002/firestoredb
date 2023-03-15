15bdb07c644a6b2dc641399a4d348f80dc82d9c3
Harish A <harish18092002@gmail.com>
2023-03-15 23:21:28 +0530

    Db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'db';
+
+  constructor(private dbs: AngularFirestore) { }
+
+  createcollection() {
+    const tutorialsRef = this.dbs.collection('studentsbd');
+    const tutorial = { name: "Harish", package: "2000", location: "chennai" };
+    tutorialsRef.add({ ...tutorial });
+  }
+
+  deletecollection() {
+    const tutorialsRef = this.dbs.collection('tutorials');
+    tutorialsRef.doc('id').delete();
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyCQQrClFGFTor3MWsHAx7tFaw7spLYGAzA",
+        authDomain: "databse-56f7c.firebaseapp.com",
+        projectId: "databse-56f7c",
+        storageBucket: "databse-56f7c.appspot.com",
+        messagingSenderId: "117705281592",
+        appId: "1:117705281592:web:7d3670ad0d9b0639f196cd"
+    },
+};
